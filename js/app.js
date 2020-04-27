1cfab42fa80083238cbecb8ecdbeb2ed0937c6cc
agianakouros <55397838+agianakouros@users.noreply.github.com>
2020-04-27 13:18:59 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,10 +17,10 @@ var ctx = canvas.getContext('2d');
 //   return ctx;
 // }
 
-// ctx.beginPath();
-// ctx.rect(0, 0, 1024, 400);
-// ctx.fillStyle = "white";
-// ctx.fill();
+ctx.beginPath();
+ctx.rect(0, 0, 1024, 400);
+ctx.fillStyle = "white";
+ctx.fill();
 // ctx.moveTo(20, 140);
 // ctx.lineTo(130, 140);
 // ctx.stroke();
