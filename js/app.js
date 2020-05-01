17f9aefdf54a344dfcc73cc6e655391bab3884b3
agianakouros <55397838+agianakouros@users.noreply.github.com>
2020-05-01 10:46:38 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+
 var alphabet;
 var wordBank;
 var attempt;
